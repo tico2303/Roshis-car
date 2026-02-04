d3d3e3ebd25e007d91f652d4463f56a698795adc
tico2303 <tico82003@gmail.com>
2026-02-04 13:05:30 -0800

    add basic urdf description

---
+++ pi/daro_ros2_ws/src/daro_description/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'daro_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='daro',
+    maintainer_email='tico82003@gmail.com',
+    description='DARO description (URDF/Xacro)',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)

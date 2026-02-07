2aa38a53cfff03e021c1165b9e4d2fc4e4405a00
tico2303 <tico82003@gmail.com>
2026-02-07 08:31:20 -0800

    add lidar and slam launch

--- pi/daro_ros2_ws/src/daro_description/setup.py
+++ pi/daro_ros2_ws/src/daro_description/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'daro_description'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+         # add URDF files into the share directory
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
